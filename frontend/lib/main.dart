4d139128e74a9ad3610516fdfe295d780580f892
ibadlayevam19 <malika19102005@gmail.com>
2025-11-12 08:34:38 +0500

    rebased and called 2 screens in main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/catalog_page.dart';
 import 'package:frontend/theme/app_theme.dart';
+import 'package:frontend/screens/my_borrowings_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [
+    const CatalogPage(),
+    const MyBorrowingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: AppTheme.lightTheme,      //  Use our custom light theme
-      darkTheme: AppTheme.darkTheme,   //  Use our custom dark theme
-      themeMode: ThemeMode.system,     //  Automatically switch by system
-      home: const CatalogPage(),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      home: Scaffold(
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Catalog'),
+            BottomNavigationBarItem(icon: Icon(Icons.info), label: 'Second'),
+          ],
+        ),
+      ),
     );
   }
 }
