0cde1d9f7bebb26c2df84611e852b94e05e29d67
saidmurod <s.xamidov@newuu.uz>
2025-11-30 00:27:01 +0500

    added create book

--- backend/src/routes/booksRoute.ts
+++ backend/src/routes/booksRoute.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getBooks, getBookByID } from "../controllers/booksController.js";
+import { getBooks, getBookByID , getBookByISBN, createBook} from "../controllers/booksController.js";
 
 const router = Router();
 
 router.get("/", getBooks);
+router.get("/isbn/:isbn", getBookByISBN);
 router.get("/:id", getBookByID);
+router.post("/", createBook);
 
 export default router;
